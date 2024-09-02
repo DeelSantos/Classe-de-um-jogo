82ac3bb31fe6ad8298368d9d83510562f49f1f8e
Edelson Santos <edelsonjr.4@hotmail.com>
2024-09-02 07:11:23 -0300

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,7 +1,27 @@
 class hero{
-    constructor(nome, idade, tipo) {
+    constructor(nome, idade, tipo, ataque, defesa) {
         this.nome = nome
         this.idade = idade
         this.tipo = tipo
+        this.ataque = ataque
+        this.defesa = defesa
+    }
+
+    atacar() {
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${this.ataque}`);
+    }
+
+    defender() {
+        console.log(`O ${this.tipo} ${this.nome} se defende usando ${this.defesa}`);
+        
     }
 }
+
+
+let heroMago = new hero("Edelson", 28, "Mago", "Bola de fogo", "Escudo da fé")
+let heroGuerreiro = new hero("Cleitinho", 18, "Guerreiro", "Tormenta de aço", "Escudo pesado")
+let heroMonge = new hero("Shogeki", 21, "Monge", "Lotus primária", "Defesa absouluta")
+
+
+
+
